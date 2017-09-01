acad04db0feaa4bf6238f5046fddf47b50f5deda
akatane <akatogeme10@gmail.com>
2017-09-02 03:50:37 +0900

    Develop minimal system

--- main.c
+++ main.c
@@ -1,10 +1,25 @@
 /* main.c */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+char input[10];
+int start;
 
 int main(int argc, char **argv){
-	printf("PRESS (S)TART\n");
-	sugoroku();
-	printf("GAME OVER\n");
+	printf("[1]START / [0]QUIT: ");
+	while (1) {
+		start = 0;
+		if (fgets(input, sizeof(input), stdin) != NULL) {
+			start = atoi(input);
+		}
+		if (start == 0) {
+			break;
+		}
+		printf("\033[2J");
+		sugoroku();
+		printf("GAME OVER\n");
+		printf("[1]RESTART / [0]QUIT: ");
+	}
 	return 0;
 }
--- sugoroku.c
+++ sugoroku.c
@@ -4,16 +4,47 @@
 #include <stdlib.h>
 #include <time.h>
 
+char input[10];
+int dice;
+int totalSquare;
+int remainSquare;
+int roll;
+
 int sugoroku(){
+	/* initialize random seed */
 	srandom((unsigned int)time(NULL));
 
-	int dice = rollDice();
-	printf("さいころの目は %d でした\n", dice);
+	/* initialize total square (41 - 50) */
+	totalSquare = random() % 10 + 41;
+	printf("今回のマス数: %d\n", totalSquare);
+
+	/* initialize remain square */
+	remainSquare = totalSquare;
+
+
+	/* goal */
+	while (remainSquare > 0) {
+		printf("ゴールまで %d マスです。\n", remainSquare);
+
+		/* display player */
+		printf("あなたの番です。\n");
+
+		/* roll dice (1 - 6) */
+		printf("[1]サイコロをふる／[0]やめる: ");
+		roll = 0;
+		if (fgets(input, sizeof(input), stdin) != NULL) {
+			roll = atoi(input);
+		}
+		if (roll == 0) {
+			return 1;
+		}
+		dice = random() % 6 + 1;
+		printf("%d が出ました。\n", dice);
+
+		/* set remain square */
+		remainSquare -= dice;
+	}
+	printf("ゴール！\n");
 
 	return 0;
-}
-
-int rollDice(){
-	int dice = random() % 6 + 1;
-	return dice;
 }
